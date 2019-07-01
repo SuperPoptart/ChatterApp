f2dcee827ccaa643977e61ab41a0110d95cb50c6
Richard Sanchez <superpoptart1@gmail.com>
2019-07-01 09:30:45 -0600

    Adding
    Adding files

---
+++ ChatterApp/chatServer/src/main/java/ChatServer.java
@@ -0,0 +1,15 @@
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+public class ChatServer {
+
+    private Selector selector = null;
+
+    private ServerSocketChannel serverSocket = null;
+
+    public ChatServer(String arg) {
+    }
+
+    public void run() {
+    }
+}
